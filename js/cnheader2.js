62493dc0b9d56a7f0435c817067e14718b072936
FantasyDDTank <89459935+FantasyDDTank@users.noreply.github.com>
2021-11-12 20:38:42 +0800

    Add files via upload

--- js/cnheader2.js
+++ js/cnheader2.js
@@ -10,3 +10,39 @@ selectElement('#title-0').addEventListener('click', () => {
 	document.getElementById('container-0').classList.toggle('rotateicon');
 
 });
+
+selectElement('#title-1').addEventListener('click', () => {
+	document.getElementById('content-1').classList.toggle('show-1');
+	document.getElementById('container-1').classList.toggle('rotateicon');
+
+});
+
+selectElement('#title-2').addEventListener('click', () => {
+	document.getElementById('content-2').classList.toggle('show-2');
+	document.getElementById('container-2').classList.toggle('rotateicon');
+
+});
+
+selectElement('#title-3').addEventListener('click', () => {
+	document.getElementById('content-3').classList.toggle('show-3');
+	document.getElementById('container-3').classList.toggle('rotateicon');
+
+});
+
+selectElement('#title-4').addEventListener('click', () => {
+	document.getElementById('content-4').classList.toggle('show-4');
+	document.getElementById('container-4').classList.toggle('rotateicon');
+
+});
+
+selectElement('#title-5').addEventListener('click', () => {
+	document.getElementById('content-5').classList.toggle('show-5');
+	document.getElementById('container-5').classList.toggle('rotateicon');
+
+});
+
+selectElement('#title-6').addEventListener('click', () => {
+	document.getElementById('content-6').classList.toggle('show-6');
+	document.getElementById('container-6').classList.toggle('rotateicon');
+
+});
